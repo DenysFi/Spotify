8ae1c887389f9e27de68aaa5719bee96f7b4941a
DenysFi <figuradenis1@gmail.com>
2024-07-22 15:07:55 +0300

    feat!: add auth

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,35 +1,56 @@
-import { createBrowserRouter } from "react-router-dom"
+import { ProtectedRoute } from "@/features/auth/components/protected-route";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import AppRoot from "./app/app-root";
+import RedirectIfAuthenticated from "@/features/auth/components/redirect-If-authenticated";
 
 export const createRouter = () => {
-    return createBrowserRouter([
+  return createBrowserRouter([
+    {
+      path: "/",
+      lazy: async () => {
+        const { LandingRoute } = await import("./landing");
+        return { Component: LandingRoute };
+      },
+    },
+    {
+      path: "/auth",
+      element: (
+        <RedirectIfAuthenticated>
+          <Outlet />
+        </RedirectIfAuthenticated>
+      ),
+      children: [
         {
-            path: '/',
-            lazy: async () => {
-                const { LandingRoute } = await import('./landing');
-                return { Component: LandingRoute };
-            },
+          path: "/auth/register",
+          lazy: async () => {
+            const { RegisterRoute } = await import("./app/auth/register-route");
+            return { Component: RegisterRoute };
+          },
         },
         {
-            path: '/register',
-            lazy: async () => {
-                const { RegisterRoute } = await import('./app/auth/register-route');
-                return { Component: RegisterRoute };
-            },
+          path: "/auth/login",
+          lazy: async () => {
+            const { LoginRoute } = await import("./app/auth/login-route");
+            return { Component: LoginRoute };
+          },
         },
-        {
-            path: '/login',
-            lazy: async () => {
-                const { LoginRoute } = await import('./app/auth/login-route');
-                return { Component: LoginRoute };
-            },
-        },
-        {
-            path: '*',
-            lazy: async () => {
-                const { NotFoundRoute } = await import('./not-found');
-                return { Component: NotFoundRoute };
-            }
-        }
+      ],
+    },
 
-    ])
-}
+    {
+      path: "*",
+      lazy: async () => {
+        const { NotFoundRoute } = await import("./not-found");
+        return { Component: NotFoundRoute };
+      },
+    },
+    {
+      path: "/app",
+      element: (
+        <ProtectedRoute>
+          <AppRoot />
+        </ProtectedRoute>
+      ),
+    },
+  ]);
+};

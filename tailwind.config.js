c6e9bd6cd76265b396c40b431504d6abae0d3893
DenysFi <figuradenis1@gmail.com>
2024-07-22 14:58:06 +0300

    Build: Add new deps and tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,64 @@
 /** @type {import('tailwindcss').Config} */
-
-
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
+    screens: {
+      mobile: "480px",
+      mdmobile: "747px",
+    },
     container: {
       center: true,
-      padding: '2rem',
+      padding: "2rem",
       screens: {
-        '2xl': '1400px',
+        "2xl": "1400px",
       },
     },
     extend: {
       fontFamily: {
-        nunito: ['Nunito', 'sans-serif'],
+        nunito: ["Nunito", "sans-serif"],
       },
       backgroundImage: {
-        'primaryBgGradient': "linear-gradient(rgba(255, 255, 255, 0.1) 0%, rgb(0, 0, 0) 100%)",
+        primaryBgGradient:
+          "linear-gradient(rgba(255, 255, 255, 0.1) 0%, rgb(0, 0, 0) 100%)",
       },
       colors: {
-        ring: 'var(--ring)',
-        primaryBg: 'var(--primary-bg)',
-
-        iconPrimaryHover: 'hsl(var(--icon-primary-foreground))',
-        iconSecondaryHover: 'hsl(var(--icon-secondaty-foreground))',
-        iconBgHover: 'hsl(var(--icon-foreground))',
+        ring: "var(--ring)",
+        primaryBg: "var(--primary-bg)",
+        "green-color": "var(--green)",
+        mainGray: "var(--main-gray)",
+        iconPrimaryHover: "hsl(var(--icon-primary-foreground))",
+        iconSecondaryHover: "hsl(var(--icon-secondaty-foreground))",
+        iconBgHover: "hsl(var(--icon-foreground))",
 
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
         textButton: {
-          DEFAULT: 'hsl(var(--text-button))',
-          hover: 'hsl(var(--text-button-hover))',
+          DEFAULT: "hsl(var(--text-button))",
+          hover: "hsl(var(--text-button-hover))",
         },
-
+      },
+      keyframes: {
+        "select-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-select-content-available-height)" },
+        },
+        "select-up": {
+          from: { height: "var(--radix-select-content-available-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "select-down": "select-down 0.2s ease-out",
+        "select-up": "select-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: [import("tailwindcss-animate")],
+};

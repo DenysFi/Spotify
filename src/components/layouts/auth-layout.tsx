ddcd1e00db0ca9f8bc13ce2bfab488a75a279740
DenysFi <figuradenis1@gmail.com>
2024-07-22 15:17:59 +0300

    chore(auth): auth layout

--- src/components/layouts/auth-layout.tsx
+++ src/components/layouts/auth-layout.tsx
@@ -1,52 +1,49 @@
-import React, { type PropsWithChildren } from 'react'
-import Head from '../seo/head'
-import clsx from 'clsx';
-import Logo from '../ui/logo/logo';
+import React, { type PropsWithChildren } from "react";
+import Head from "../seo/head";
+import Logo from "../ui/logo/logo";
+import { cn } from "@/utils/cn";
 
 interface AuthLayoutProps extends PropsWithChildren {
-    type: 'login' | 'registrate'
+  type: "login" | "registrate";
 }
 
 function Header() {
-
-    return (
-        <div className='pt-8 pb-6'>
-            <Logo color='white' />
-        </div>
-    )
+  return (
+    <div className="pt-8 pb-6">
+      <Logo color="white" />
+    </div>
+  );
 }
 
 function AuthLayout({ children, type }: AuthLayoutProps) {
+  const headTitle = type.charAt(0).toUpperCase() + type.slice(1);
+  const isLogin = type === "login";
 
-    const headTitle = type.charAt(0).toUpperCase() + type.slice(1);
-    const isLogin = type === 'login';
+  return (
+    <section
+      className={cn(
+        "h-full absolute w-full bg-primaryBg flex flex-col items-center ",
+        {
+          "mdmobile:bg-primaryBgGradient mdmobile:p-8": isLogin,
+        }
+      )}
+    >
+      <Head title={`${headTitle} | Spotify`} />
+      {!isLogin && (
+        <>
+          <Header />
+          {children}
+        </>
+      )}
 
-    return (
-        <section className={
-            clsx(
-                'w-screen h-screen bg-primaryBg flex flex-col items-center ',
-                {
-                    'sm:bg-primaryBgGradient sm:p-8': isLogin,
-                }
-            )}>
-
-            <Head title={`${headTitle} | Spotify`} />
-            {!isLogin && (
-                <>
-                    <Header />
-                    {children}
-                </>
-            )}
-
-            {isLogin &&
-                <div className='max-w-[45em] w-full bg-primaryBg flex flex-col items-center rounded-lg pb-8 '>
-                    <Header />
-                    {children}
-                </div>
-            }
-
-        </section>
-    )
+      {isLogin && (
+        <div className="max-w-[45em] w-full bg-primaryBg flex flex-col items-center mdmobile:rounded-lg pb-8 px-4">
+          <Header />
+          {children}
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default AuthLayout
+export default AuthLayout;

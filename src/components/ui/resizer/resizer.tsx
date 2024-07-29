6e5ed97f362374cac2feb4e157b27d150c964516
DenysFi <figuradenis1@gmail.com>
2024-07-29 14:35:01 +0300

    feat: add resizer component

---
+++ src/components/ui/resizer/resizer.tsx
@@ -0,0 +1,68 @@
+import { cn } from "@/utils/cn"
+import { type HtmlHTMLAttributes, type ReactNode } from "react"
+
+import "./resizer.scss"
+import { useResizable, type UseResizableProps } from "./useResizable"
+
+type ResizerProps = HtmlHTMLAttributes<HTMLDivElement> &
+	UseResizableProps & {
+		children: ReactNode
+		className?: string
+	}
+
+function Resizer({
+	min,
+	max,
+	id,
+	saveLastWidth,
+	position,
+	children,
+	className,
+	...props
+}: ResizerProps) {
+	const { contentRef, resizerRef, setCurrentWidth, isResizing, currentWidth } =
+		useResizable({
+			min,
+			max,
+			id,
+			saveLastWidth,
+			position,
+		})
+
+	return (
+		<div
+			ref={contentRef}
+			className={cn("relative inline-block", className)}
+			{...props}
+		>
+			<div
+				ref={resizerRef}
+				className={cn(
+					" absolute flex items-center justify-center  h-full top-0 resizer-outline focus-within:after:opacity-100 focus-within:after:bg-[var(--resizer-bg-active)] after:h-[calc(100%-16px)] after:transition-all after:opacity-0 after:hover:opacity-100 duration-700 after:bg-[var(--resizer-bg-hover)] z-1000 cursor-grab w-[var(--panel-gap)] after:content-[''] after:w-[1px] ",
+					{
+						"start-[calc(var(--panel-gap)*-1)]": position === "left",
+						"end-[calc(var(--panel-gap)*-1)]": position === "right",
+						"after:opacity-100 after:bg-[var(--resizer-bg-active)] cursor-grabbing":
+							isResizing,
+					}
+				)}
+			>
+				<label className="w-[1px] h-[1px] margin-[-1px] overflow-hidden p-0 absolute border-0">
+					Изменить размер главной панели навигации
+					<input
+						type="range"
+						max={max}
+						min={min}
+						step={10}
+						dir={position === "left" ? "rtl" : "ltr"}
+						onChange={e => setCurrentWidth(parseInt(e.target.value))}
+						value={currentWidth}
+					/>
+				</label>
+			</div>
+			{children}
+		</div>
+	)
+}
+
+export default Resizer

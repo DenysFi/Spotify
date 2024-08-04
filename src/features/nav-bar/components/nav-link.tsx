98a5284458e2d2f45d76ec659d70ee426c02f899
DenysFi <figuradenis1@gmail.com>
2024-08-04 23:50:01 +0300

    feat: navigation link component

---
+++ src/features/nav-bar/components/nav-link.tsx
@@ -0,0 +1,45 @@
+import {
+	NavLink as RouterNavLink,
+	type NavLinkProps as RouterNavLinkProps,
+} from "react-router-dom"
+
+import { forwardRef, type ReactNode } from "react"
+import { buttonVariants } from "../../../components/ui/button"
+
+import { cn } from "@/utils/cn"
+import type { VariantProps } from "class-variance-authority"
+
+export type NavLinkProps = RouterNavLinkProps &
+	VariantProps<typeof buttonVariants> & {
+		icon?: ReactNode
+		children: ReactNode
+	}
+
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
+	(
+		{ className, size, variant, hover, children, icon = null, ...props },
+		ref
+	) => {
+		return (
+			<RouterNavLink
+				ref={ref}
+				// tabIndex={1}
+				className={({ isActive }) =>
+					cn(
+						buttonVariants({ size, variant, hover, className }),
+						"font-extrabold px-3",
+						{ "text-white": isActive }
+					)
+				}
+				{...props}
+			>
+				<span className="flex gap-4">
+					{icon}
+					{children}
+				</span>
+			</RouterNavLink>
+		)
+	}
+)
+
+export default NavLink

077f3e6d982f759e02c0c94513f5c448f9b27b0f
DenysFi <figuradenis1@gmail.com>
2024-08-30 20:59:18 +0300

    refactor: query

--- src/lib/client-query.ts
+++ src/lib/client-query.ts
@@ -1,14 +1,19 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query"
 
 const queryOptions = {
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            retry: false,
-            slateTime: 60 * 1000,
-
-        },
-    },
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: false,
+			slateTime: 60 * 1000 * 60 * 24,
+		},
+	},
 }
 
 export const queryClient = new QueryClient(queryOptions)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+	ReturnType<T>,
+	"queryKey" | "queryFn"
+>

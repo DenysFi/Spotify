5a624b544ce015c6858d38463092156c58ee739a
DenysFi <figuradenis1@gmail.com>
2024-09-10 17:39:23 +0300

    chore: table data generic as type

--- src/features/playlists/compontents/tracks-table.tsx
+++ src/features/playlists/compontents/tracks-table.tsx
@@ -4,34 +4,36 @@ import {
 	TableElement,
 	TableHeader,
 	TableRow,
+	tableRowVariants,
 } from "@/components/ui/table/table"
 import React from "react"
-import type { TrackItemsType } from "../api/get-playlists"
 import { TableTrackSkeleton } from "./table-track-skeleton"
+import { type VariantProps } from "class-variance-authority"
 
-interface TableColumns {
+interface TableColumns<T> {
 	title: React.ReactNode
 	field: string
-	Cell: ({
-		entry,
-		index,
-	}: {
-		entry: TrackItemsType
-		index: number
-	}) => React.ReactNode
+	Cell: ({ entry, index }: { entry: T; index: number }) => React.ReactNode
 }
 
-interface TracksTableProps {
-	data: TrackItemsType[] | undefined
-	columns: TableColumns[]
+type TracksTableProps<T> = {
+	data: T[] | undefined
+	columns: TableColumns<T>[]
 	isLoading: boolean
-}
+	ids: string[] | undefined
+} & VariantProps<typeof tableRowVariants>
 
-function TracksTable({ data, columns, isLoading }: TracksTableProps) {
+function TracksTable<T>({
+	data,
+	ids,
+	grid,
+	columns,
+	isLoading,
+}: TracksTableProps<T>) {
 	return (
 		<TableElement>
 			<TableHeader>
-				<TableRow className=" h-full items-center">
+				<TableRow grid={grid} className="h-full items-center ">
 					{columns.map(({ field, title }) => (
 						<TableCell key={field}>{title}</TableCell>
 					))}
@@ -39,10 +41,10 @@ function TracksTable({ data, columns, isLoading }: TracksTableProps) {
 			</TableHeader>
 			<TableBody>
 				{data?.map((entry, index) => (
-					<TableRow key={entry.track.id + index} className="h-14 ">
+					<TableRow key={ids![index] + index} grid={grid} className="h-14 ">
 						{columns.map(({ field, title, Cell }) => (
-							<TableCell key={field + title} className="relative">
-								<Cell entry={entry} index={index + 1} />
+							<TableCell key={field + title} className="relative ">
+								<Cell entry={entry} index={index} />
 							</TableCell>
 						))}
 					</TableRow>

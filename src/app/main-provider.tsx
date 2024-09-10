97935d852e589ad926351ea2cf36bb0e327cd4f4
DenysFi <figuradenis1@gmail.com>
2024-09-10 17:27:09 +0300

    Style(prettier): format code

--- src/app/main-provider.tsx
+++ src/app/main-provider.tsx
@@ -1,24 +1,24 @@
-import MainErrorFallback from "@/components/errors/main-error-falback";
-import AuthProvider from "@/features/auth/context/auth-context";
-import { queryClient } from "@/lib/client-query";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { HelmetProvider } from "react-helmet-async";
+import MainErrorFallback from "@/components/errors/main-error-falback"
+import AuthProvider from "@/features/auth/context/auth-context"
+import { queryClient } from "@/lib/client-query"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import React from "react"
+import { ErrorBoundary } from "react-error-boundary"
+import { HelmetProvider } from "react-helmet-async"
 
 function AppProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <React.Suspense>
-      <ErrorBoundary fallback={<MainErrorFallback />}>
-        <HelmetProvider>
-          <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools />
-            <AuthProvider>{children}</AuthProvider>
-          </QueryClientProvider>
-        </HelmetProvider>
-      </ErrorBoundary>
-    </React.Suspense>
-  );
+	return (
+		<React.Suspense>
+			<ErrorBoundary fallback={<MainErrorFallback />}>
+				<HelmetProvider>
+					<QueryClientProvider client={queryClient}>
+						<ReactQueryDevtools />
+						<AuthProvider>{children}</AuthProvider>
+					</QueryClientProvider>
+				</HelmetProvider>
+			</ErrorBoundary>
+		</React.Suspense>
+	)
 }
-export default AppProvider;
+export default AppProvider

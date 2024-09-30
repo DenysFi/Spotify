9be4795ad341ff30d4281d1db12c875694664d1d
DenysFi <figuradenis1@gmail.com>
2024-09-30 23:02:04 +0300

    refactor: code optimization

--- src/utils/tracks-utils.ts
+++ src/utils/tracks-utils.ts
@@ -1,23 +1,24 @@
 import type { GetArtistsReturn } from "@/features/media-library/api/get-recently-listened"
+import type { GetPlaylistTrackReturn } from "@/features/playlists/api/get-playlist-tracks"
 import type { TrackItemsType } from "@/features/playlists/api/get-playlists"
 
 export type ArtistType = Pick<GetArtistsReturn, "id" | "name" | "type">
 
-export function joinArtists(artists: ArtistType[] | undefined) {
-	if (!artists) return ""
-	return artists.map(artist => artist.name).join(", ")
+export function joinArtists(artists?: ArtistType[]) {
+	return artists?.map(artist => artist.name).join(", ") ?? ""
 }
 
-export function concatPages(pages: any) {
-	return pages?.reduce((acc, page) => {
-		return [...acc, ...page.items]
-	}, []) as TrackItemsType[] | undefined
+export function concatPages(pages: GetPlaylistTrackReturn[] = []) {
+	return pages.reduce(
+		(acc, page) => [...acc, ...page.items],
+		[] as TrackItemsType[]
+	)
 }
 
 export type GetTrackDurationProps = {
 	duration_ms: string
 }
 
-export function getTrackDuration(tracks: GetTrackDurationProps[] | undefined) {
-	return tracks?.reduce((acc, item) => (acc += +item.duration_ms), 0) || 0
+export function getTrackDuration(tracks: GetTrackDurationProps[] = []) {
+	return tracks.reduce((acc, item) => (acc += +item.duration_ms), 0)
 }

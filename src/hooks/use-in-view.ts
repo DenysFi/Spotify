bc39867e4c7d05a7d3425c1ada86086f28cd9330
DenysFi <figuradenis1@gmail.com>
2024-09-30 23:01:14 +0300

    fix: node type

--- src/hooks/use-in-view.ts
+++ src/hooks/use-in-view.ts
@@ -1,16 +1,16 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useState } from "react";
 
 export function useInView() {
-	const [inView, setInView] = useState(false)
+  const [inView, setInView] = useState(false);
 
-	const ref = useCallback(node => {
-		if (!node) return
+  const ref = useCallback((node: HTMLElement | null) => {
+    if (!node) return;
 
-		const observer = new IntersectionObserver(([entry]) => {
-			setInView(entry.isIntersecting)
-		})
-		observer.observe(node)
-	}, [])
+    const observer = new IntersectionObserver(([entry]) => {
+      setInView(entry.isIntersecting);
+    });
+    observer.observe(node);
+  }, []);
 
-	return { ref, inView }
+  return { ref, inView };
 }
